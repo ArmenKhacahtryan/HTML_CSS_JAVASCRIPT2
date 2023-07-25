7e8fc472f6cc15ab425d1d54b569281a5883122a
ArmenKhacahtryan <139985596+ArmenKhacahtryan@users.noreply.github.com>
2023-07-25 17:58:07 +0400

    Add files via upload

---
+++ it code/js/index.js
@@ -0,0 +1,57 @@
+function myFunction() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+function openForm() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("overlay-bg").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("overlay-bg").style.display = "none";
+}
+const colors = ["#FFDC02", "#191913", "#FFDC02", "#FFDC02", "#FFDC02"];
+
+const numBalls = 80;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 61)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000,
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
